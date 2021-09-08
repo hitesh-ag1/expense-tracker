0a5e06e24d7883845ba6e4989437fb712e599407
Hitesh Agarwal <44574125+hiteshsdata@users.noreply.github.com>
2021-09-08 17:32:37 +0800

    updated aggregate functions

--- todayFuncs.py
+++ todayFuncs.py
@@ -9,27 +9,33 @@ def todayFilter(data):
 def transToday(data):
     if (len(data) == 0):
         return (data.to_dict())
+    data.loc[((data.payee == '') & (data.type=='Deposit')), 'payee'] = 'ATM Deposit'
     trans = (data)[['amt', 'payee', 'type']]
     trans.loc[trans.type == 'Payment', 'amt'] = np.negative(trans.loc[trans.type == 'Payment', 'amt']).values
     trans = trans.groupby('payee',).amt.sum()
     trans = trans.to_dict()
+    trans = {k: round(v, 2) for k, v in trans.items()}
     return trans
 
 def transTodaySummarybyType(data):
     if (len(data) == 0):
         return (data.to_dict())
+    data.loc[((data.payee == '') & (data.type=='Deposit')), 'payee'] = 'ATM Deposit'
     da = (data)[['amt', 'type']]
-    da = ((da.groupby([pd.Grouper(freq='D'), 'type']).amt.sum()).droplevel('date')).to_dict()
+    da = ((da.groupby(['type']).amt.sum())).to_dict()
+    da = {k: round(v, 2) for k, v in da.items()}
     return da
 
 def transTodaySummarybyCat(data):
     if (len(data) == 0):
         return (data.to_dict())
+    data.loc[((data.payee == '') & (data.type=='Deposit')), 'payee'] = 'ATM Deposit'
     da = (data)[['amt', 'category', 'type']]
-    da = ((da.groupby([pd.Grouper(freq='D'), 'category', 'type']).amt.sum()).droplevel('date'))
+    da = ((da.groupby([ 'category', 'type']).amt.sum()))
     da.loc[da.index.get_level_values('type') == 'Payment'] = np.negative(da.loc[da.index.get_level_values('type') == 'Payment'])
     da = da.droplevel('type')
     da = da.to_dict()
+    da = {k: round(v, 2) for k, v in da.items()}
     return da
     # summary = data.groupby([pd.Grouper(freq='D'), 'category', 'type']).amt.sum()
     # day_summary = todayMultiIndexFilter(summary)
